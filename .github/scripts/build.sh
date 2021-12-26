694d87099268b9307e04e9dcaa59d073b9a31ee9
Carlos Gonzalez <cyberpolin@gmail.com>
2021-12-26 16:05:35 -0600

    test with ok names

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,6 +1,6 @@
-cd /Users/cyberpolin/projects/entre/self-hosted-ios-build/ios
+cd /Users/cyberpolin/projects/entre/self-hosted-ios-build/app/ios
 echo 'Starting'
-xcodebuild -workspace Calculator.xcworkspace \
-            -scheme Calculator\ iOS \
+xcodebuild -workspace app.xcworkspace \
+            -scheme app\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
