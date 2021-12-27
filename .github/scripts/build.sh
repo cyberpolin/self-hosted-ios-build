d04c354b9d3d764943e8b06015ed9498b55f2f89
Carlos Gonzalez <cyberpolin@gmail.com>
2021-12-26 21:21:48 -0600

    getting close

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,5 +1,5 @@
 echo 'Starting'
 xcodebuild -workspace ./app/ios/app.xcworkspace \
-            -scheme app\ iOS \
+            -scheme app \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test
