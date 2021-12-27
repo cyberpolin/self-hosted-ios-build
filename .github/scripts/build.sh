4afb80278641ebe315f6a9f4d226f6166c4f6c29
Carlos Gonzalez <cyberpolin@gmail.com>
2021-12-26 21:15:04 -0600

    fix scode path

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,5 +1,5 @@
 echo 'Starting'
-xcodebuild -workspace ./app/app.xcworkspace \
+xcodebuild -workspace ./app/ios/app.xcworkspace \
             -scheme app\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
