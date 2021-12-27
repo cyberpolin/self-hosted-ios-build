23137d27500bfc1130c4b2e571df76dc90cae2d8
Carlos Gonzalez <cyberpolin@gmail.com>
2021-12-26 21:16:48 -0600

    remove xcpretty

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -2,4 +2,4 @@ echo 'Starting'
 xcodebuild -workspace ./app/ios/app.xcworkspace \
             -scheme app\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
-            clean test | xcpretty
+            clean test
